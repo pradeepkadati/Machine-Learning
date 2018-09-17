44d5591138468056885e8f6c9365d987e4ff7486
pradeepkadati <pradeepkadati@gmail.com>
2018-09-17 07:50:31 +0530

    Pre-Processing Updated

--- Data-Pre-Processing/DataProcessing.py
+++ Data-Pre-Processing/DataProcessing.py
@@ -43,3 +43,9 @@ y = labelEnc_y.fit_transform(y)
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(X,y,test_size =0.2,random_state =0)
 
+# Scallng up the data
+
+from sklearn.preprocessing import StandardScaler
+sc_x = StandardScaler()
+x_train = sc_x.fit_transform(x_train)
+x_test =  sc_x.transform(x_test)
